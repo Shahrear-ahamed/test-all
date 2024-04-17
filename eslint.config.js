52857b9608fa9944bec1d9f5ee682b4b149a7208
Shahrear Ahamed <its.shahrear@gmail.com>
2024-04-17 10:16:10 +0600

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import tsParser from '@typescript-eslint/parser'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['node_modules', 'dist', '.env'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['src/**/*.ts'],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      'prefer-const': 'error',
+      'no-unused-expressions': 'error',
+      'no-undef': 'error',
+      'no-console': 'off',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+      },
+      globals: {
+        process: 'readonly',
+      },
+    },
+    extends: [eslintConfigPrettier],
+  },
+)
